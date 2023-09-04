20d5ab0e1982d3195dab41ad3bba702a5181df02
Odwori1 <odwori.okuku@gmail.com>
2023-09-04 05:49:06 -0700

    tests matrices multiplication function

---
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""Write imported module here"""
+
+
+def matrix_mul(m_a, m_b):
+    """Func that multiplies two matrices
+
+    Args:
+        m_a: the first matrix
+        m_b: the second matrix
+
+    Raises:
+        TypeError:m_a or m_b is not a list , m list of lists, if elements are
+               not int or float, or inner lists are not of the same len
+
+        ValueError: if lists are empty, or items cannot be multiplied
+
+    Returns: the product matrix
+    """
+    if not isinstance(m_a, list):
+        raise TypeError("m_a must be a list")
+    if not isinstance(m_b, list):
+        raise TypeError("m_b must be a list")
+
+    if not all(isinstance(row, list) for row in m_a):
+        raise TypeError("m_a must be a list of lists")
+    if not all(isinstance(row, list) for row in m_b):
+        raise TypeError("m_b must be a list of lists")
+
+    if m_a == [] or m_a == [[]]:
+        raise ValueError("m_a can't be empty")
+    if m_b == [] or m_a == [[]]:
+        raise ValueError("m_b can't be empty")
+
+    for row in m_a:
+        for element in row:
+            if not isinstance(element, (int, float)):
+                raise TypeError("m_a should contain only integers or floats")
+    for row in m_b:
+        for element in row:
+            if not isinstance(element, (int, float)):
+                raise TypeError("m_b should contain only integers or floats")
+
+    if not all(len(row) == len(m_a[0]) for row in m_a):
+        raise TypeError("each row of m_a must be of the same size")
+    if not all(len(row) == len(m_b[0]) for row in m_b):
+        raise TypeError("each row of m_b must be of the same size")
+
+    if len(m_a[0]) != len(m_b):
+        raise ValueError("m_a and m_b can't be multiplied")
+
+    result = [[0] * len(m_b[0]) for _ in range(len(m_a))]
+    for i in range(len(m_a)):
+        for j in range(len(m_b[0])):
+            for k in range(len(m_b)):
+                result[i][j] += m_a[i][k] * m_b[k][j]
+
+    return result
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testfile("tests/100-matrix_mul.txt")

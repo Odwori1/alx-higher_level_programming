bff9d661b0dc102bdc9a125a7097739ea8bc7592
Odwori1 <odwori.okuku@gmail.com>
2023-09-04 19:45:10 -0700

    a class Rectangle that defines a rectangle by: (based on 4-rectangle.py)

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Write imported module here"""
+
+
+class Rectangle:
+    """Represents a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """ "Initializes the rectangle
+
+        Args:
+            width: width of the rect
+            height: height of the rect
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """getter method of the width
+
+        Returns: the width
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setter method for the width
+
+        Args:
+            value: width's value
+
+        Raises:
+            TypeError: if width is not an integer
+            ValueError: if width less than 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """getter method of the height

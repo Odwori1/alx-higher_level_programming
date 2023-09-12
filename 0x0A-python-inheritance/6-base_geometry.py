9d3c68dcec8c4b1fc55ebdf82e54c709a6861965
Odwori1 <odwori.okuku@gmail.com>
2023-09-11 19:34:01 -0700

    class BaseGeometry (based on 5-base_geometry.py).

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Write imported module here"""
+
+
+class BaseGeometry:
+    """The BaseGeometry class"""
+    def area(self):
+        """Func that calculate area"""
+        raise Exception('area() is not implemented')

6b65abec0e396a77131c2ccc0240b42b63942e15
Odwori1 <odwori.okuku@gmail.com>
2023-09-12 19:29:59 -0700

    class

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Imported module for add_item"""
+
+
+class Student:
+    """a student class"""
+    def __init__(self, first_name, last_name, age):
+        """constructor method"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """retrieves a dictionary representation"""
+        return self.__dict__.copy()

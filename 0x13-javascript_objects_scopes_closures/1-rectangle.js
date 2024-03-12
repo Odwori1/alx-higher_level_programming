10219c8f65621b8efe8562c279662e3dbaf3db27
Odwori1 <odwori.okuku@gmail.com>
2024-03-12 10:00:20 -0700

    Rectangle representation

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-//function represents the Rectangle class
-
-class Rectangle{
-	constructor(w,h){
-		this.width = w;
-		this.height = h;	
+// Represents the Rectangle class
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
 }
 
-}
 module.exports = Rectangle;

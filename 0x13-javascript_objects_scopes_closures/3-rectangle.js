4fd0b9ae5dff22262f701c13cf3443b95e8ce57c
Odwori1 <odwori.okuku@gmail.com>
2024-03-12 11:09:47 -0700

    3-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// Represents the Rectangle class
+class Rectangle {
+  constructor (w, h) {
+    if ((w = parseInt(w)) > 0 && (h = parseInt(h)) > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let idx = 0; idx < this.height; idx++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Rectangle;

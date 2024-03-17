29cf1a9970f0bc9f181613a1882f396aa20e3370
Odwori1 <odwori.okuku@gmail.com>
2024-03-17 03:31:45 -0700

    model fetchall

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Class definition of a State"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
+
+Base = declarative_base()
+
+
+class State(Base):
+    """ ORM class for state """
+    __tablename__ = "states"
+    id = Column(
+        Integer,
+        primary_key=True,
+        nullable=False,
+        autoincrement=True,
+        unique=True
+        )
+    name = Column(String(128), nullable=False)
+    cities = relationship(
+        "City",
+        cascade="all, delete-orphan",
+        backref=backref("state", cascade="all"),
+        single_parent=True)
